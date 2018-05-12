984e038493c980f705cab2fd6e99e990da5b63a7
Robert Wolf <wolfrob35@gmail.com>
2018-05-12 17:03:13 +0200

    preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,4 +3,4 @@ import CMS from 'netlify-cms'
 import ReferencePreview from './preview-templates/ReferencePreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('reference', ReferencePreview)
+CMS.registerPreviewTemplate('references', ReferencePreview)
